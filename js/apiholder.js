2fe219e73801b04a9ad9f50a54f1d5e33b0bf033
rileytfindley <ctf69220@uga.edu>
2019-12-12 18:57:58 -0500

    got article displaying

---
+++ js/apiholder.js
@@ -0,0 +1,1 @@
+let newsapi = "f6321734306a4d48af50f68e7d24dd83";
--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
-/*
-  Please add all Javascript code to this file.
-*/
+//onLoad
+//ajax or fetch or .get to load api
+
+function onLoad() {
+  site = $.ajax({
+    url:"https://newsapi.org/v2/top-headlines?sources=CNN&apiKey=" + newsapi,
+    type: 'GET',
+    data: {
+      format: 'json'
+    },
+    success: (results) => {
+      console.log(results);
+      results.articles.forEach(function(result, index){
+        impressionsEl.innerHTML = index + 1;
+        let title = result.title;
+        titleEl.innerHTML = title;
+        let content = result.description;
+        contentEl.innerHTML = content;
+        let url = result.url;
+        //urlEl.innerHTML = url;
+        document.getElementById(urlEl).href = url;
+        let image = result.urlToImage; //set up to src in feedr
+        document.getElementById(imageEl).src = image;
+        //$("ul").append("<li>"+result.content.title+"</li>")
+        //break;
+      })
+    }
+  })
+  // $.get("https://newsapi.org/v2/top-headlines?sources=CNN&apiKey=" + newsapi, function(results){
+  //   console.log(results);
+  //   results.data.feed.forEach(function(result){
+  //     let title = result.title;
+  //     titleEl.innerHTML = title;
+  //     let content = result.description;
+  //     contentEl.innerHTML = content;
+  //     let url = result.url;
+  //     document.getElementById(urlEl).href = url;
+  //     let image = result.urlToImage; //set up to src in feedr
+  //     document.getElementById(imageEl).src = image;
+  //     $("ul").append("<li>"+result.content.title+"</li>")
+  //   })
+  // })
+}
+window.onload = onLoad;
